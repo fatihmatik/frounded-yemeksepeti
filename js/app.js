e1dba9b4fb9cdf868e7155d143d38f5e9a443c87
Fatih Tezcan <123652272+fatihmatik@users.noreply.github.com>
2024-03-26 17:09:56 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,19 @@
+function togglePopupLogin(){
+    var popup = document.getElementById("loginpopup");
+    if (popup.style.display === 'flex') {
+        popup.style.display = 'none';
+    }
+    else {
+        popup.style.display = 'flex';
+    }
+};
+
+function closePopupLogin(){
+    var popup = document.getElementById("loginpopup");
+    if (popup.style.display === 'flex') {
+        popup.style.display = 'none';
+    }
+    else {
+        popup.style.display = 'flex';
+    }
+};
